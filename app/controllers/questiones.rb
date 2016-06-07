f4fcdc94f45ba464f0a54b497ae58520e3bbcb2b
codeamac14 <codeamac14@gmail.com>
2016-06-07 17:59:10 -0500

    add questions with answers

--- app/controllers/questiones.rb
+++ app/controllers/questiones.rb
@@ -1,18 +1,16 @@
 post '/new_question/question/:id' do
-  id = params[:id]
+  id_survey = params[:id]
   question = params[:question]
   answer1 = params[:answer1]
   answer2 = params[:answer2]
   answer3 = params[:answer3]
   answer4 = params[:answer4]
-  p question
-  p "****"
-  p answer1
-  p "****"
-  p answer2
-  p "****"
-  p answer3
-  p "****"
-  p answer4
-  redirect to "/edit/survey/#{id}"
+  Question.create(survey_id: id_survey, question: question)
+  pregunta = Question.last
+  Answer.create(question_id: pregunta.id, answer: answer1)
+  Answer.create(question_id: pregunta.id, answer: answer2)
+  Answer.create(question_id: pregunta.id, answer: answer3)
+  Answer.create(question_id: pregunta.id, answer: answer4)
+  
+  redirect to "/edit/survey/#{id_survey}"
 end
